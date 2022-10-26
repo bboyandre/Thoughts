9366e84e4c65d0bf64fdc8411a112c16cab00b15
andrem3 <https://andrem3@bitbucket.org/>
2022-10-25 22:28:10 -0300

    first commit

---
+++ controllers/ThoughtsController.js
@@ -0,0 +1,8 @@
+const Thought = require('../models/Thought')
+const User = require('../models/User')
+
+module.exports = class ThoughtsController {
+    static async showThoughts(req, res) {
+        res.render('thoughts/home')
+    }
+}
